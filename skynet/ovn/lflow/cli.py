e7989c2efb02b7bcc9df26b1c303b43ae3d542ef
Adrian Moreno <amorenoz@redhat.com>
2020-11-19 15:47:02 +0100

    lflow: add filtering
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- skynet/ovn/lflow/cli.py
+++ skynet/ovn/lflow/cli.py
@@ -1,7 +1,7 @@
 import click
 
 from skynet.context import SkyNetCtxt
-from skynet.ovn.lflow.data import LFlowProvider
+from skynet.ovn.lflow.data import LFlowProvider, LFlowFilter
 
 
 @click.group(name='lflow')
@@ -15,11 +15,24 @@ def lflowcli(obj: SkyNetCtxt) -> None:
 
 @lflowcli.command()
 @click.pass_obj
-def list(obj: SkyNetCtxt) -> None:
+@click.argument('filter', required=False, default="")
+def list(obj: SkyNetCtxt, filter: str = "") -> None:
     """
     List Logical Flows
+    \b
+    FILTER is a Filter string formatted as "Filter1=Value1,Filter2=Value2,..."
+    Supported Filters:
+        Datapath    [datapath] (full or first 8bytes)
+        Table       [tableNum] (table number)
+    E.g: Datapath=c51244a5-1620-4a7a-ae1e-c4b882e46aae
+         Datapath=c51244a5,Table=12
     """
-    print(
-        LFlowProvider(obj).get().data().to_string(
-            columns=['Match', 'Actions', 'Pipeline','Priority', 'Table', 'DataPath'],
-            justify="left",max_colwidth=65))
+    filter_obj = LFlowFilter()
+    filter_obj.process_string(filter)
+    flow_data = LFlowProvider(obj).get(filter_obj)
+    if not flow_data.is_empty():
+        print(flow_data.data().to_string(columns=[
+            'Match', 'Actions', 'Pipeline', 'Priority', 'Table', 'Datapath'
+        ],
+                                         justify="left",
+                                         max_colwidth=60))
--- skynet/ovn/lflow/data.py
+++ skynet/ovn/lflow/data.py
@@ -1,8 +1,7 @@
 from typing import Dict, List, Any
-from pandas import DataFrame
 
 from skynet.context import SkyNetCtxt
-from skynet.common.data import SkyDiveData, Metadata, SkyDiveDataProvider
+from skynet.common.data import SkyDiveData, Metadata, SkyDiveDataProvider, SkyDiveFilter, SkyDiveDataFilter
 
 
 class LFlowData(SkyDiveData):
@@ -14,7 +13,8 @@ class LFlowData(SkyDiveData):
         Metadata('OVN.LFPriority', None, 'Priority'),
         Metadata('OVN.Pipeline', None, 'Pipeline'),
         Metadata('OVN.Table', None, 'Table'),
-        Metadata('OVN.LogicalDataPath', lambda x: x[0:8] if len(x) > 0 else None, 'DataPath'),
+        Metadata('OVN.LogicalDataPath', lambda x: x[0:8]
+                 if len(x) > 0 else None, 'Datapath'),
     ]
     """
     LFlowData represents Logical Flow Data
@@ -24,8 +24,21 @@ class LFlowData(SkyDiveData):
         LFlowData constructor
         """
         super(LFlowData, self).__init__(data=data,
-                                     meta=self.METADATA,
-                                     index="Name")
+                                        meta=self.METADATA,
+                                        index="Name")
+
+
+class LFlowFilter(SkyDiveDataFilter):
+    """
+    Logical Flow filters
+    """
+    def __init__(self):
+        filters = [
+            SkyDiveFilter("Table", int, "OVN.Table"),
+            SkyDiveFilter("Datapath", lambda x: "Regex('{}.*')".format(x),
+                          "OVN.LogicalDataPath")
+        ]
+        super(LFlowFilter, self).__init__(filters)
 
 
 class LFlowProvider(SkyDiveDataProvider):
@@ -38,10 +51,13 @@ class LFlowProvider(SkyDiveDataProvider):
         """
         super(LFlowProvider, self).__init__(ctxt=ctxt)
 
-    def get(self) -> LFlowData:
+    def get(self, filter_obj: LFlowFilter = None) -> LFlowData:
+
+        gremlin_filter = filter_obj.generate_gremlin() if filter_obj else ""
         at = "At('%s')." % self._ctxt.options().get(
             'at') if self._ctxt.options().get('at') else ''
 
-        query = "g.{at}V().Has('Type', 'logical_flow')".format(at=at)
+        query = "g.{at}V().Has('Type', 'logical_flow'){filt}".format(
+            at=at, filt=gremlin_filter)
         data = self._run_query(query)
         return LFlowData(data)
