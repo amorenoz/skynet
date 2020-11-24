7f4600f46ef5384ced4ddc9ae051f1488535a370
Adrian Moreno <amorenoz@redhat.com>
2020-11-24 16:22:46 +0100

    Add bridge module
    Apart from the common, boilerplate data representations, add a couple of
    aggretagion classes:
      OvSPort aggregats ports and its Interfaces
      OvSBridge aggregates a bridge and its ports
    
    Add a "get" function that prints the OvS Bridge Details. Propably not
    the best way to print the information but, it's something
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

---
+++ skynet/ovs/bridge/cli.py
@@ -0,0 +1,64 @@
+import click
+import json
+import textwrap
+
+from skynet.context import SkyNetCtxt
+from skynet.ovs.bridge.data import OvSDataProvider
+
+
+@click.group(name='bridge')
+@click.pass_obj
+def bridgecli(obj: SkyNetCtxt) -> None:
+    """
+    OvS Bridge commands
+    """
+    pass
+
+
+@bridgecli.command()
+@click.option('-h',
+              '--host',
+              'host',
+              help='Only list the OvS Bridges on a host'
+              '(Either name or UUID are acceptable values)')
+@click.pass_obj
+def list(obj: SkyNetCtxt, host: str) -> None:
+    """
+    List OvS Bridges
+    """
+    print(
+        OvSDataProvider(obj).list_bridges(host).to_string(
+            columns=['Host', 'Name', 'BridgeID', 'BridgeUplink']))
+
+
+@bridgecli.command(name='get')
+@click.argument('bridge', required=True)
+@click.pass_obj
+def get_bridge(obj: SkyNetCtxt, bridge: str) -> None:
+    """
+    Get detail information from a bridge
+    """
+    iface_info = {
+        'internal': ['Type', 'Name', 'IPV4'],
+        'patch': ['Type', 'Name', 'Peer', 'LocalPort'],
+        'geneve':
+        ['Type', 'Name', 'MTU', 'TunEgressIface', 'TunEgressIfaceCarrier'],
+        'veth': [
+            'Type', 'Name', 'State', 'IPAddress', 'AttachedMAC', 'Sandbox',
+            'IfaceID'
+        ],
+    }
+    indent_str = ' '
+
+    ovsbridge = OvSDataProvider(obj).get_bridge(bridge)
+    print(ovsbridge.bridge.data()[['Name', 'BridgeID']].iloc[0])
+    print("Ports:")
+    for port in ovsbridge.ports:
+        print(
+            textwrap.indent(port.port.data()[['Name']].iloc[0].to_string(),
+                            indent_str * 4))
+        print(textwrap.indent("Interfaces:", indent_str * 4))
+
+        for uid, iface in port.ifaces.data()[['Type']].iterrows():
+            series = port.ifaces.data()[iface_info[iface['Type']]].loc[uid]
+            print(textwrap.indent(series.to_string(), indent_str * 8))
